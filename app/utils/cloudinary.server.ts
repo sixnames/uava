2ae970c8d691dc93fed10f6e59ac779b82537c3e
sixnames <slava.kirshman@icloud.com>
2022-03-29 15:03:58 +0300

    $avatarFileName[.]png.tsx route added

--- app/utils/cloudinary.server.ts
+++ app/utils/cloudinary.server.ts
@@ -10,7 +10,7 @@ cloudinary.v2.config({
   api_secret: process.env.API_SECRET,
 });
 
-async function getBufferFromUrl(url: string): Promise<Buffer> {
+export async function getBufferFromUrl(url: string): Promise<Buffer> {
   return new Promise((resolve) => {
     https.get(url, (response) => {
       const body: Buffer[] = [];
@@ -150,4 +150,55 @@ export async function generateAvatar(
 
     transformedStream.pipe(uploader);
   });
+}
+
+export async function generateAvatarBuffer(
+  args: GenerateAvatarOptionsInterface,
+): Promise<Buffer | null> {
+  // get avatar buffer
+  const avatarUrl = await getCloudinaryImageUrl(args.avatarFileName);
+  const avatarBuffer = await getBufferFromUrl(avatarUrl);
+
+  // create a sharp image from the avatar buffer
+  const width = Math.floor(args.width);
+  const height = Math.floor(args.height);
+  const avaBuffer = await sharp(avatarBuffer)
+    .resize(width, height)
+    .extract({
+      left: Math.floor(args.x),
+      top: Math.floor(args.y),
+      width: avaSize,
+      height: avaSize,
+    })
+    .composite([
+      {
+        input: getCircleMask(avaSize),
+        gravity: 'center',
+        blend: 'dest-in',
+      },
+    ])
+    .toFormat('png')
+    .toBuffer();
+
+  // get flag buffer
+  const flagUrl = await getCloudinaryImageUrl('ua-flag.png');
+  const flagBuffer = await getBufferFromUrl(flagUrl);
+
+  // composite the avatar image with the ua flag
+  return sharp(flagBuffer)
+    .resize(flagSize, flagSize)
+    .composite([
+      {
+        input: getCircleMask(flagSize),
+        gravity: 'center',
+        blend: 'dest-in',
+      },
+      {
+        input: avaBuffer,
+        gravity: 'center',
+        blend: 'over',
+      },
+    ])
+    .toFormat('png')
+    .toBuffer();
 }
